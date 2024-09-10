8c03a359c6573297ae3736252a61ed7a0cf9cda9
Om Patel <[97650590+om0611@users.noreply.github.com]>
2024-09-10 11:52:22 -0400

    Implemented hover effect and added reset button

--- main.js
+++ main.js
@@ -9,7 +9,22 @@ for (let i=1; i <= 16; i++) {
         boxDiv = document.createElement('div');
         boxDiv.classList.add('square');
         boxDiv.style.cssText = `box-sizing: border-box; border: 1px solid black; height: ${squareSize}px; width: ${squareSize}px;`;
+        boxDiv.addEventListener('mouseenter', function(e) {
+            e.target.style.backgroundColor = "black";
+        });
         rowDiv.appendChild(boxDiv);
     }
     container.appendChild(rowDiv);
+}
+
+let resetButton = document.createElement("button");
+resetButton.textContent = "Reset";
+resetButton.addEventListener('click', resetGrid);
+document.body.appendChild(resetButton);
+
+function resetGrid() {
+    let boxes = document.querySelectorAll('.square');
+    boxes.forEach((box) => {
+        box.style.backgroundColor = 'white';
+    });
 }
